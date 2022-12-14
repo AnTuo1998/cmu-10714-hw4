2e009208f7bd4c06ae99a92befa87cc37e5be53c
dushangchen <dscupup@gmail.com>
2022-12-14 15:15:32 -0800

    init

--- python/needle/init.py
+++ python/needle/init.py
@@ -68,27 +68,56 @@ def ones_like(array, *, device=None, requires_grad=False):
     )
 
 
+def _calculate_fan_in_and_fan_out(shape):
+    dimensions = len(shape)
+    if dimensions < 2:
+        raise ValueError(
+            "Fan in and fan out can not be computed for tensor with fewer than 2 dimensions")
+
+    num_input_fmaps = shape[-2]
+    num_output_fmaps = shape[-1]
+    receptive_field_size = 1
+    if dimensions > 2:
+        # math.prod is not always available, accumulate the product manually
+        # we could use functools.reduce but that is not supported by TorchScript
+        for s in shape[:-2]:
+            receptive_field_size *= s
+
+    fan_in = num_input_fmaps * receptive_field_size
+    fan_out = num_output_fmaps * receptive_field_size
+    print(fan_in, fan_out, receptive_field_size)
+    return fan_in, fan_out
+
+
 def xavier_uniform(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    a = gain * math.sqrt(6.0 / float(fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-a, high=a, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = gain * math.sqrt(2.0 / float(fan_in + fan_out))
+    return randn(fan_in, fan_out, mean=0., std=std, **kwargs)
     ### END YOUR SOLUTION
 
 
 def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)
+    bound = gain * math.sqrt(3.0 / fan_in)
+    shape = shape or (fan_in, fan_out)
+    return rand(*shape, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 
 def kaiming_normal(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)
+    std = gain / math.sqrt(fan_in)
+    shape = shape or (fan_in, fan_out)
+    return randn(*shape, mean=0, std=std, **kwargs)
     ### END YOUR SOLUTION
